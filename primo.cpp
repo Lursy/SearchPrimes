572792e252a1c9055c23b659076c1b9773e4582a
Lursy <70035797+Lursy@users.noreply.github.com>
2023-09-26 18:15:55 -0300

    Update primo.cpp

--- primo.cpp
+++ primo.cpp
@@ -3,12 +3,10 @@
 #include <cmath>
 #include <ctime>
 
-// Crivo de Atkin
 std::vector<bool> is_prime(int limit) {
     std::vector<bool> primes(limit + 1, true);
     primes[0] = primes[1] = false;
 
-    // Only check for divisors up to the square root of the limit.
     for (int i = 2; i * i <= limit; ++i) {
         if (primes[i]) {
             for (int j = i * i; j <= limit; j += i) {
@@ -17,7 +15,6 @@ std::vector<bool> is_prime(int limit) {
         }
     }
 
-    // Mark the squares as composite.
     for (int i = 3; i * i <= limit; i += 2) {
         if (primes[i]) {
             for (int j = i * i; j <= limit; j += i * i) {
@@ -33,7 +30,6 @@ int main() {
     long int limit = 10000000;
     // scanf("%ld", &limit);
 
-    // Initialize the clock.
     auto start = std::chrono::steady_clock::now();
 
     std::vector<bool> primes = is_prime(limit);
@@ -46,7 +42,6 @@ int main() {
         }
     }
 
-    // Use a more efficient sorting algorithm.
     std::sort(prime_numbers.begin(), prime_numbers.end());
 
     // Print the list of prime numbers.
